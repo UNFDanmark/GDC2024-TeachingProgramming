8a84ac165230d106207913614c22f412a9c1dfc0
161836499+TheDavidNN@users.noreply.github.com <161836499+TheDavidNN@users.noreply.github.com>
2024-07-12 15:18:42 +0200

    Opgave 2

--- Undervisning/Assets/PlayerMover.cs
+++ Undervisning/Assets/PlayerMover.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    public int tal = 2;
+    public GameObject player;
+
+    public Transform t;
+    
+    public Vector3 v;
+
+    public PlayerMover pm;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
+
+       
+        float kommatal = 0.5f;
+        string tekst = "tekst";
+        bool tjek = true;
+        
+        print(kommatal);
+        print(tekst);
+        print(tjek);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Debug.Log("Update");
+        print(tal);
     }
 }
